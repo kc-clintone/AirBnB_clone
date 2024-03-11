bd2d6f454a9c5e4350e44f54397ff16a2e2cb131
kc-clintone <kayseeclintone@gmail.com>
2024-03-11 11:10:37 +0000

    AirBnB_clone project >>> task 3

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Makes this folder a package"""
 
-from models.engine.file_storage import FileStorage
+#from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
