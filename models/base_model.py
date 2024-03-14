7da489b265c74fb5c77e99614658871fa8802923
kc-clintone <kayseeclintone@gmail.com>
2024-03-14 20:54:27 +0000

    AirBnB_clone project >>> corrections and tidying up

--- models/base_model.py
+++ models/base_model.py
@@ -27,10 +27,10 @@ class BaseModel:
                         kwargs["updated_at"], std_format)
                 else:
                     self.__dict__[x] = kwargs[x]
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
        """
