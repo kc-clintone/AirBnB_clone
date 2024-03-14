25e590d200c6d8ce9a7b6b98cd713a5a262efae4
kc-clintone <kayseeclintone@gmail.com>
2024-03-14 05:58:36 +0000

    AirBnB_clone project >>> corrections and tidying up

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,9 @@ import uuid
 class BaseModel:
     """This is going to be the base class that all others will inherit"""
     def __init__(self):
-    """
-    Initializes the objects.
-    """
+        """
+        Initializes the objects.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
