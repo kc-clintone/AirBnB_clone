564930909730888977dd1403a852c785ec5cd4ac
kc-clintone <kayseeclintone@gmail.com>
2024-03-14 06:00:34 +0000

    AirBnB_clone project >>> corrections and tidying up

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
         save key/value paira to __dict__
         """
         new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class.__name__
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
