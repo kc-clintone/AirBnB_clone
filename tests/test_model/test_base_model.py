b9ba51d9461b9c9b5701488d511df686b5c916f2
kc-clintone <kayseeclintone@gmail.com>
2024-03-14 20:15:06 +0000

    AirBnB_clone project >>> corrections and tidying up

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -13,8 +13,24 @@ class TestBaseModel(unittest.TestCase):
         """testing the save method"""
         new_model = BaseModel()
         changes = new_model.updated_at
-        write_changes = new_model.save()
-        self.assertNotEqual(changes, write_changes)
+        save_changes = new_model.save()
+        self.assertNotEqual(changes, save_changes)
 
     def init(self):
     """testing initializing method"""
+        test_model = BaseModel()
+        self.assertIsNotNone(test_model.id)
+        self.assertIsNotNone(test_model.created_at)
+        self.assertIsNotNone(test_model.updated_at)
+
+    def to_dict(self):
+    """testing dictionary method"""
+        test_model = BaseModel()
+        test_dict = test_model.to_dict()
+        self.assertIsInstance(test_dict, dict)
+
+        self.assertEqual(test_dict["__class__"], "BaseModel")
+        self.assertEqual(test_dict["id"], test_model.id)
+        self.assertEqual(test_dict["created_at"], test_model.created_at.isoformat())
+        self.assertEqual(test_dict["updated_at"], test_model.updated_at.isoformat())
+
